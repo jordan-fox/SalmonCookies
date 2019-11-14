a43a265217d857b94eb5a5c93d44fc9b8d510b00
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-11-13 20:26:19 -0800

    I have thoroughly destroyed the code, I look forward to Front Row watching again.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,9 @@
 'use strict';
 
-var hoursArray = [
-  '6am',
-  '7am',
-  '8am',
-  '9am',
-  '10am',
-  '11am',
-  '12pm',
-  '1pm',
-  '2pm',
-  '3pm',
-  '4pm',
-  '5pm',
-  '6pm',
-  '7pm',
-];
 
-var storeSales = document.getElementById('stores');
+// var storeSales = document.getElementById('stores');
+
+
 
 function Store(storeName, minCustomersPerHour, maxCustomersPerHour, averageCookiesPerCustomer, customersEachHourArray, cookiesSoldEachHourArray, totalCookiesTheDay) {
   this.storeName = storeName;
@@ -27,40 +13,68 @@ function Store(storeName, minCustomersPerHour, maxCustomersPerHour, averageCooki
   this.customersEachHourArray = customersEachHourArray;
   this.cookiesSoldEachHourArray = cookiesSoldEachHourArray;
   this.totalCookiesTheDay = 0;
+
+  // Store.push(this);
 }
 
-  calculateCustomers: function() {
+console.log(Store);
+
+function NumberCustomers() {
+  this.NumberCustomers = function() {
     for(var i = 0 ; i < hoursArray.length ; i++) {
       var randomCustomer = getRandom(this.minCustomersPerHour, this.maxCustomersPerHour);
       this.customersEachHourArray.push(randomCustomer);
     }
-  },
-
-  calculateCookies: function() {
-    for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
-      var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
-      this.totalCookiesTheDay += cookies;
-      this.cookiesSoldEachHourArray.push(cookies);
-    }
-  },
-
-  render: function() {
-    this.calculateCustomers(),
-    this.calculateCookies();
-    var ulEl = document.createElement('ul');
-    var h2El = document.createElement('h2');
-    h2El.textContent = this.storeName;
-    storeSales.appendChild(h2El);
-    for( var i = 0; i < hoursArray.length; i++ ) {
-      var liEl = document.createElement('li');
-      liEl.textContent = `${hoursArray[i]}: ${this.cookiesSoldEachHourArray[i]} Cookies`;
-      ulEl.appendChild(liEl);
-    }
-    liEl = document.createElement('li');
-    liEl.textContent = `Total: ${this.totalCookiesTheDay} Cookies`;
-    ulEl.appendChild(liEl);
-    storeSales.appendChild(ulEl);
   };
+
+  NumberCustomers.push(this);
+}
+
+console.log(NumberCustomers);
+
+// calculateCustomers: function() {
+//   for(var i = 0 ; i < hoursArray.length ; i++) {
+//     var randomCustomer = getRandom(this.minCustomersPerHour, this.maxCustomersPerHour);
+//     this.customersEachHourArray.push(randomCustomer);
+//   }
+// };
+
+function CalculateCookies() {
+  for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
+    var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
+    this.totalCookiesTheDay += cookies;
+    this.cookiesSoldEachHourArray.push(cookies);
+  }
+  CalculateCookies.push(this);
+}
+
+console.log(CalculateCookies);
+
+// calculateCookies: function() {
+//   for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
+//     var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
+//     this.totalCookiesTheDay += cookies;
+//     this.cookiesSoldEachHourArray.push(cookies);
+//   }
+// };
+
+// render: function() {
+//   this.calculateCustomers(),
+//   this.calculateCookies();
+//   var ulEl = document.createElement('ul');
+//   var h2El = document.createElement('h2');
+//   h2El.textContent = this.storeName;
+//   storeSales.appendChild(h2El);
+//   for( var i = 0; i < hoursArray.length; i++ ) {
+//     var liEl = document.createElement('li');
+//     liEl.textContent = `${hoursArray[i]}: ${this.cookiesSoldEachHourArray[i]} Cookies`;
+//     ulEl.appendChild(liEl);
+//   }
+//   liEl = document.createElement('li');
+//   liEl.textContent = `Total: ${this.totalCookiesTheDay} Cookies`;
+//   ulEl.appendChild(liEl);
+//   storeSales.appendChild(ulEl);
+// };
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
