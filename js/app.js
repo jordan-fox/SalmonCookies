b895fda59a3454b29c3a88627858e735e8f68d91
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-11-13 17:06:28 -0800

    Added Paris to js file

--- js/app.js
+++ js/app.js
@@ -193,6 +193,49 @@ var dubai = {
   }
 };
 
+var paris = {
+  storeName: 'Paris Headquarters',
+  minCustomersPerHour: 20,
+  maxCustomersPerHour: 38,
+  averageCookiesPerCustomer: 2.3,
+  customersEachHourArray: [],
+  cookiesSoldEachHourArray: [],
+  totalCookiesTheDay: 0,
+
+  calculateCustomers: function() {
+    for(var i = 0 ; i < hoursArray.length ; i++) {
+      var randomCustomer = getRandom(this.minCustomersPerHour, this.maxCustomersPerHour);
+      this.customersEachHourArray.push(randomCustomer);
+    }
+  },
+
+  calculateCookies: function() {
+    for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
+      var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
+      this.totalCookiesTheDay += cookies;
+      this.cookiesSoldEachHourArray.push(cookies);
+    }
+  },
+
+  render: function() {
+    this.calculateCustomers(),
+    this.calculateCookies();
+    var ulEl = document.createElement('ul');
+    var h2El = document.createElement('h2');
+    h2El.textContent = this.storeName;
+    storeSales.appendChild(h2El);
+    for( var i = 0; i < hoursArray.length; i++ ) {
+      var liEl = document.createElement('li');
+      liEl.textContent = `${hoursArray[i]}: ${this.cookiesSoldEachHourArray[i]} Cookies`;
+      ulEl.appendChild(liEl);
+    }
+    liEl = document.createElement('li');
+    liEl.textContent = `Total: ${this.totalCookiesTheDay} Cookies`;
+    ulEl.appendChild(liEl);
+    storeSales.appendChild(ulEl);
+  }
+};
+
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
 }
@@ -202,3 +245,4 @@ seattle.render();
 lima.render();
 tokyo.render();
 dubai.render();
+paris.render();
