27c43fb7dc49867dba5458931a6830ba9ed1ba04
Jordan Fox <jordanmichaelfox@hotmail.com>
2019-11-14 18:31:56 -0800

    Refactored to include prototypes and working constructor functions.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
 'use strict';
 
+// Global Stuff...
 
-// var storeSales = document.getElementById('stores');
+var hoursArray = [
+  '6 a.m.',
+  '7 a.m.',
+];
 
+var storeSales = document.getElementById('stores');
 
+var allStores = [];
 
-function Store(storeName, minCustomersPerHour, maxCustomersPerHour, averageCookiesPerCustomer, customersEachHourArray, cookiesSoldEachHourArray, totalCookiesTheDay) {
+function Store(storeName, minCustomersPerHour, maxCustomersPerHour, averageCookiesPerCustomer) {
   this.storeName = storeName;
   this.minCustomersPerHour = minCustomersPerHour;
   this.maxCustomersPerHour = maxCustomersPerHour;
   this.averageCookiesPerCustomer = averageCookiesPerCustomer;
-  this.customersEachHourArray = customersEachHourArray;
-  this.cookiesSoldEachHourArray = cookiesSoldEachHourArray;
+  this.customersEachHourArray = [];
+  this.cookiesSoldEachHourArray = [];
   this.totalCookiesTheDay = 0;
 
-  // Store.push(this);
+  allStores.push(this);
 }
 
 console.log(Store);
@@ -32,49 +38,37 @@ function NumberCustomers() {
 
 console.log(NumberCustomers);
 
-// calculateCustomers: function() {
-//   for(var i = 0 ; i < hoursArray.length ; i++) {
-//     var randomCustomer = getRandom(this.minCustomersPerHour, this.maxCustomersPerHour);
-//     this.customersEachHourArray.push(randomCustomer);
-//   }
-// };
-
-function CalculateCookies() {
-  for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
-    var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
-    this.totalCookiesTheDay += cookies;
-    this.cookiesSoldEachHourArray.push(cookies);
+Store.prototype.calculateCustomers = function () {
+  for(var i = 0 ; i < hoursArray.length ; i++) {
+    var randomCustomer = getRandom(this.minCustomersPerHour, this.maxCustomersPerHour);
+    this.customersEachHourArray.push(randomCustomer);
   }
-  CalculateCookies.push(this);
-}
+};
 
-console.log(CalculateCookies);
+Store.prototype.calculateCookies = function () {
+  for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
+    var temp = Math.floor(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
+    this.cookiesSoldEachHourArray.push(temp);
+  }
+};
 
-// calculateCookies: function() {
-//   for(var i = 0 ; i < this.customersEachHourArray.length ; i++){
-//     var cookies = Math.round(this.customersEachHourArray[i] * this.averageCookiesPerCustomer);
-//     this.totalCookiesTheDay += cookies;
-//     this.cookiesSoldEachHourArray.push(cookies);
-//   }
-// };
-
-// render: function() {
-//   this.calculateCustomers(),
-//   this.calculateCookies();
-//   var ulEl = document.createElement('ul');
-//   var h2El = document.createElement('h2');
-//   h2El.textContent = this.storeName;
-//   storeSales.appendChild(h2El);
-//   for( var i = 0; i < hoursArray.length; i++ ) {
-//     var liEl = document.createElement('li');
-//     liEl.textContent = `${hoursArray[i]}: ${this.cookiesSoldEachHourArray[i]} Cookies`;
-//     ulEl.appendChild(liEl);
-//   }
-//   liEl = document.createElement('li');
-//   liEl.textContent = `Total: ${this.totalCookiesTheDay} Cookies`;
-//   ulEl.appendChild(liEl);
-//   storeSales.appendChild(ulEl);
-// };
+Store.prototype.render = function () {
+  this.calculateCustomers(),
+  this.calculateCookies();
+  var ulEl = document.createElement('ul');
+  var h2El = document.createElement('h2');
+  h2El.textContent = this.storeName;
+  storeSales.appendChild(h2El);
+  for( var i = 0; i < hoursArray.length; i++ ) {
+    var liEl = document.createElement('li');
+    liEl.textContent = `${hoursArray[i]}: ${this.cookiesSoldEachHourArray[i]} Cookies`;
+    ulEl.appendChild(liEl);
+  }
+  liEl = document.createElement('li');
+  liEl.textContent = `Total: ${this.totalCookiesTheDay} Cookies`;
+  ulEl.appendChild(liEl);
+  storeSales.appendChild(ulEl);
+};
 
 function getRandom(min, max) {
   return Math.floor(Math.random() * (max - min +1)) + min;
@@ -92,3 +86,10 @@ new Store('Lima', 2, 65, 4.6);
 new Store('Tokyo', 3, 24, 1.2);
 new Store('Dubai', 11, 38, 3.7);
 new Store('Paris', 20, 38, 2.3);
+
+
+
+for (var i = 0 ; i < allStores.length ; i++ ) {
+  allStores[i].render();
+}
+
